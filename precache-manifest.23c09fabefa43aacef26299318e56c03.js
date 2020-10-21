ea0f07533477ffa00bb10216bf7a842ea5154a89
Hector Pascual <hector.pascual.diaz@gmail.com>
2020-10-21 13:28:00 +0200

    Updates

---
+++ precache-manifest.23c09fabefa43aacef26299318e56c03.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988cf73d1c45c5941a1f34b4038599ad",
+    "url": "/chat-me/index.html"
+  },
+  {
+    "revision": "2aebddc9afcb64389191",
+    "url": "/chat-me/static/css/main.f366547b.chunk.css"
+  },
+  {
+    "revision": "9df647fa33e3c105c919",
+    "url": "/chat-me/static/js/2.ae095c90.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/chat-me/static/js/2.ae095c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aebddc9afcb64389191",
+    "url": "/chat-me/static/js/main.96eed747.chunk.js"
+  },
+  {
+    "revision": "fd9004f4f40592aa7aec",
+    "url": "/chat-me/static/js/runtime-main.b04c9b8e.js"
+  },
+  {
+    "revision": "92b043a0031eefa10d7dfd06e8a9654f",
+    "url": "/chat-me/static/media/carles.92b043a0.jpg"
+  },
+  {
+    "revision": "4deeeeb08d3d9fc7d488ac52a46633b7",
+    "url": "/chat-me/static/media/chatbg.4deeeeb0.png"
+  },
+  {
+    "revision": "211c1153d75a5941a214cc29314c222f",
+    "url": "/chat-me/static/media/done_all.211c1153.svg"
+  },
+  {
+    "revision": "958bf042217447a5282c358d5c23ff03",
+    "url": "/chat-me/static/media/mic.958bf042.svg"
+  },
+  {
+    "revision": "231fed550308fbb034a7b92d0a12bb52",
+    "url": "/chat-me/static/media/search.231fed55.svg"
+  },
+  {
+    "revision": "c50cc4490e6bcb8a32f3b77676df03be",
+    "url": "/chat-me/static/media/tag_faces.c50cc449.svg"
+  }
+]);
